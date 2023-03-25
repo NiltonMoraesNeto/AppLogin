18ba77db78c02fcab27e14a2c41f8a682238b4cd
Nilton Moraes Neto <nilton.nmn@gmail.com>
2023-03-24 23:13:51 -0300

    Ajustes finais do App

--- App.js
+++ App.js
@@ -5,17 +5,19 @@ import {
   View,
   Image,
   TextInput,
-  TouchableOpacity, Platform
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback
 } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function App() {
   return (
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
     <View style={styles.container}>
       <StatusBar backgroundColor="#FFF" translucent={false} />
-      <Image
-        source={require("./src/assets/logo1.png")}
-        style={styles.logo}
-      />
+      <Image source={require("./src/assets/logo1.png")} style={styles.logo} />
       <TextInput
         placeholder="Celular, username ou email"
         style={styles.input}
@@ -29,7 +31,26 @@ export default function App() {
       <TouchableOpacity style={styles.loginButton}>
         <Text style={styles.loginText}>Acessar</Text>
       </TouchableOpacity>
+      <TouchableOpacity style={styles.facebookContainer}>
+        <FontAwesome5 name="facebook" size={25} color="#4484ec" />
+        <Text style={styles.facebookText}>Logar com Facebook</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.googleContainer}>
+        <FontAwesome5 name="google" size={25} color="#34a354" />
+        <Text style={styles.googleText}>Logar com Google</Text>
+      </TouchableOpacity>
+      <View style={styles.divisor}>
+        <View style={styles.divisorLine}></View>
+        <Text style={{ marginHorizontal: "3%" }}>OU</Text>
+        <View style={styles.divisorLine}></View>
+      </View>
+      <View style={styles.signUpContainer}>
+        <Text style={styles.signUpText}>Não possui uma conta?</Text>
+        <TouchableOpacity><Text style={styles.signUpButton}>Cadastrar</Text></TouchableOpacity>
+      </View>
     </View>
+    </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -40,13 +61,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   logo: {
-    marginTop: Platform.OS === 'android' ? '13%' : '20%',
-    marginBottom: Platform.OS === 'android' ? '13%' : '15%',
+    marginTop: Platform.OS === "android" ? "13%" : "20%",
+    marginBottom: Platform.OS === "android" ? "13%" : "15%",
     width: 100,
     height: 100,
   },
   input: {
-    width: '90%',
+    width: "90%",
     height: 42,
     backgroundColor: "#F4F3F3",
     marginBottom: 20,
@@ -55,16 +76,16 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#E0E0E0",
   },
-  forgotContainer:{
-    width: '90%',
+  forgotContainer: {
+    width: "90%",
     alignItems: "flex-end",
   },
-  forgotText:{
-    color: "#dc7c58"
+  forgotText: {
+    color: "#dc7c58",
   },
   loginButton: {
-    marginTop: '8%',
-    width: '90%',
+    marginTop: "8%",
+    width: "90%",
     height: 42,
     backgroundColor: "#dc7c58",
     borderRadius: 5,
@@ -75,4 +96,50 @@ const styles = StyleSheet.create({
     color: "#FFF",
     fontSize: 16,
   },
+  facebookContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: "15%",
+  },
+  googleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: "5%",
+  },
+  facebookText: {
+    color: "#4484ec",
+    paddingLeft: 8,
+    fontSize: 15,
+  },
+  googleText: {
+    color: "#34a354",
+    paddingLeft: 8,
+    fontSize: 15,
+    marginRight: "4%",
+  },
+  divisor: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "10%",
+    width: "90%",
+  },
+  divisorLine: {
+    width: "45%",
+    height: 2,
+    backgroundColor: "#EFEDED",
+    borderRadius: 5,
+  },
+  signUpContainer: {
+    flexDirection: "row",
+    marginTop: "10%",
+  },
+  signUpText: {
+    color: "#dc7c58",
+    paddingRight: 5,
+  },
+  signUpButton: {
+    color: "#f3b304",
+    fontWeight: "bold",
+  }
 });
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AppLogin
 Tela de login mobile, feito com React Native(RN)
+
+
+<p align="center">
+  <img src="https://uploaddeimagens.com.br/images/004/406/490/original/WhatsApp_Image_2023-03-24_at_23.04.17.jpeg?1679710371" width="350" title="hover text">
+</p>
